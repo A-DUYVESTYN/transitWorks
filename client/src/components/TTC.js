a732f2964ee82bf8deb56260be55c30a6d3e63a7
A-DUYVESTYN <a.duyvestyn@gmail.com>
2022-11-29 20:50:11 -0500

    style TTC notices in single component

--- client/src/components/TTC.js
+++ client/src/components/TTC.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import React from "react";
+// const classNames = require('classnames');
 
 function TTC(props) {
 
@@ -9,18 +10,28 @@ function TTC(props) {
     twitterId: "19025957",
     tweetList: [],
   });
-  const routes = [1,2,3,4,301,304,306,310,501,503,504,505,506,509,510,511,512]
-// bus routes: 7 to 189, 300 to 396 (blue night), 400-405 (community bus), 900 - 996 (express)
+  // const routes = [1,2,3,4,301,304,306,310,501,503,504,505,506,509,510,511,512]
+  // bus routes not included in routes array: 7 to 189, 300 to 396 (blue night), 400-405 (community bus), 900 - 996 (express)
 
   const formatTweets = (tweetArr) => {
-    // remove the link url from end of tweet text
-    
+    const getRouteColor = function (num) {
+      if (num === 1) return '[#f8c300]'
+      if (num === 2) return '[#00923f]'
+      if (num === 3) return '[#0082c9]'
+      if (num === 4) return '[#a21a68]'
+      if (num < 300) return '[#da251d]'
+      if (num < 400) return '[#024182]'
+      if (num < 500) return '[#808080]'
+      if (num < 900) return '[#da251d]'
+      if (num > 900) return '[#00923f]'
+    }
     const formattedArr = [];
     tweetArr.forEach((element) => {
-      const tweetText = element.text.slice(0, element.text.indexOf(" http"));
-      // if (tweetText.length > maxLength) tweetText = tweetText.substring(0, maxLength) + "..." // cap tweet length to 125 char
-      const routeNumber = 2
-      const routeType = "Line 1"
+      const tweetText = element.text.slice(0, element.text.indexOf(" http")); // remove the link url from end of tweet text
+      const routeNumber = 504
+      const routeType = "Line 2"
+      const routeColor = `bg-${getRouteColor(routeNumber)}`
+      console.log(routeColor)
 
       formattedArr.push({
         created_at: element.created_at,
@@ -28,12 +39,11 @@ function TTC(props) {
         text: tweetText,
         routeNumber,
         routeType,
+        routeColor
       });
     });
-
     return formattedArr;
   };
-
 
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_SERVER_URL}/transit`)
@@ -53,15 +63,15 @@ function TTC(props) {
     <div className="flex-col">
       <h1>TTC</h1>
       <section>
-        {tweets.tweetList.map((item, index) => {
+        {tweets.tweetList.map((tweet, index) => {
           return (
-          <div className="w-full m-2 p-1 rounded-lg shadow-md border-black border-2 lg:max-w-lg">
-            <article className="space-y-2">
-              <h3 className="text-2xl font-semibold">
-                  Title
-              </h3>
+          <div className="w-full m-2 rounded-lg shadow-md border-black border-2 lg:max-w-lg">
+            <article className="flex flex-row items-start m-1">
+              <div className={`text-2xl font-semibold self-center px-4 py-2 ${tweet.routeColor}`}>
+                {tweet.routeNumber}
+              </div>
               <div key={index} id={"tweet" + index}>
-                <p>{item.text}</p>
+                <p>{tweet.text}</p>
               </div>
             </article>
           </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Line1': '#f8c300',
+        'Line2': '#00923f',
+        'Line3': '#0082c9',
+        'Line4': '#a21a68',
+        'bus': '#da251d',
+        'blueNight': '#024182',
+        '400SeriesBus': '#808080',
+        'streetcar': '#da251d',
+        'expressBus': '#00923f'
+      },
+    },
   },
   plugins: [],
 }
