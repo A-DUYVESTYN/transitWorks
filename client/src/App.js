f3a96610bc1b3e3f0bc67b30e1982ae93ed8e52c
A-DUYVESTYN <a.duyvestyn@gmail.com>
2022-12-07 18:08:39 -0500

    add dark mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,40 @@
 // import logo from './logo.svg';
 import './App.css';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TTC from "./components/TTC";
 import GoTransit from "./components/GoTransit";
 
 
 function App() {
-
   const [devView, setDevView] = useState(false)
   const toggleDevView = () => {
     console.log("changing devView=", devView, " to ", !devView)
     setDevView(!devView)
   }
 
+  const [theme, setTheme] = useState(null)
+  const handleThemeSwitch = () => {
+    setTheme(theme === "dark" ? "light" : "dark")
+  }
+  useEffect(() => {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) setTheme('dark');
+    else setTheme('light');
+  }, [])
+  useEffect(() => {
+    if (theme === "dark") document.documentElement.classList.add("dark")
+    else document.documentElement.classList.remove("dark")
+  }, [theme])
+  
   return (
-    <div className="flex flex-col mb-10">
+    <div className="flex flex-col mb-10 dark:bg-slate-800">
 
       <div className="grow">
         <h1 className="p-2 text-xl">
           Transit Service Disruptions
         </h1>
         <div className="flex flex-col md:flex-row justify-evenly">
-          <TTC devView={devView} className="w-60"/>
-          <GoTransit devView={devView} className="w-60"/>
+          <TTC devView={devView} className="w-60" />
+          <GoTransit devView={devView} className="w-60" />
         </div>
       </div>
 
@@ -42,6 +54,13 @@ function App() {
             <span className="hover:underline">Contact</span>
           </li>
         </ul>
+        <div className="flex flex-wrap h-6 items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+          <label className="inline-flex relative items-center cursor-pointer">
+            <input type="checkbox" value="" onClick={handleThemeSwitch} className="sr-only peer" />
+            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            <span className="ml-2 place-self-end mr-8 md:mr-6">Dark Mode</span>
+          </label>
+        </div>
       </footer>
     </div>
   );
